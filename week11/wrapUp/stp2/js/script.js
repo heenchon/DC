f77edfd778c715f89d16bf20deaa6907b0daf415
heenchon <101083384+heenchon@users.noreply.github.com>
2023-05-30 07:39:01 +0900

    11주차 실습

---
+++ week11/wrapUp/stp2/js/script.js
@@ -0,0 +1,11 @@
+// let aCard = document.querySelector(".card");
+// console.log(aCard);
+// aCard.onclick = (e) => {
+//   console.log("target", e.target);
+//   console.log("currentTarget", e.currentTarget);
+//   e.currentTarget.classList.toggle(
+//     "card-selected"
+//   );
+// };
+// let elem = document.getElementById("card1");
+// elem.onclick;
